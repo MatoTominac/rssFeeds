7d89ab39cd38e10c7c3bc9730f8fd6387206817c
Mato Tominac <mato.tominac@gmail.com>
2015-02-24 16:52:24 +0100

    Views

--- listFeeds/views.py
+++ listFeeds/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from listFeeds.models import Feed
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    feeds = Feed.objects.all()
+    output = ', '.join([f.feed_url for f in feeds])
+    return HttpResponse(output)
